d9b72112e5304d01946aff1a6a5a090598816e07
Sergio Teacher <Sergio Teacher@MINIWIN>
2024-01-26 17:00:45 +0100

    Agregar archivos de proyecto.

---
+++ Sergioteacher.Csharp04.Inputbox/MainWindow.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Sergioteacher.Csharp04.Inputbox
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public WindowInput ventanaInput;
+        public string data;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            ventanaInput = new WindowInput(this);
+
+
+        }
+
+        private void BIntro_Click(object sender, RoutedEventArgs e)
+        {
+            ventanaInput.Show();
+            this.Hide();
+        }
+
+        private void VentanaPrincipal_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            e.Cancel = true;
+            Application.Current.Shutdown();
+        }
+
+        private void VentanaPrincipal_Activated(object sender, EventArgs e)
+        {
+            Tsalida.Text = data;
+        }
+    }
+}
---
+++ Sergioteacher.Csharp04.Inputbox/WindowInput.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Sergioteacher.Csharp04.Inputbox
+{
+    /// <summary>
+    /// Lógica de interacción para WindowInput.xaml
+    /// </summary>
+    public partial class WindowInput : Window
+    {
+        MainWindow Inicio;
+        public WindowInput(MainWindow ventanaInicio)
+        {
+            Inicio = ventanaInicio;
+            InitializeComponent();
+            
+        }
+
+        private void BIntro_Click(object sender, RoutedEventArgs e)
+        {
+            Inicio.data = MiTexto.Text;
+            Inicio.Show();
+            this.Hide();
+        }
+
+        private void VentanaInput_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            if (this.Visibility == Visibility.Visible)
+            {
+                //MessageBox.Show("Hola, mensaje al capturar ->  X");
+                e.Cancel = true;
+                Inicio.data = "";
+                Inicio.Show();
+                this.Hide();
+            }
+        }
+
+        private void VentanaInput_Activated(object sender, EventArgs e)
+        {
+            MiTexto.Text = "";
+        }
+    }
+}
